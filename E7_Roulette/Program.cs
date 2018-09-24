a8c410b4dc653149abdfb878ba464a42e41335a0
Auston Hefling <auston.hefling@outlook.com>
2018-09-24 08:53:42 -0400

    Add project files.

---
+++ E7_Roulette/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E7_Roulette
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
